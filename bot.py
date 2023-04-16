3ef4d283cd7cae09a723db50db43282a5b111234
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2023-04-15 20:53:12 -0600

    contains hi

--- bot.py
+++ bot.py
@@ -230,7 +230,7 @@ async def on_message(message):
                     await message.channel.send(f'Here\'s your meme:\n{meme_url}')
                 else:
                     await message.channel.send("Failed to fetch a random Sonic meme.")
-            elif content_lower.contains('hi') or content_lower.contains('hello') or content_lower.contains('hey'):
+            elif 'hi' in content_lower or 'hello' in content_lower or 'hey' in content_lower:
                   await message.channel.send(f"Hey {message.author.id}!")
             else:
                 print('analyzing... ',end='')
